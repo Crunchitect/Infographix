0daee46a1ea8b1579cddf604b6f1220c6b037334
PremPremPremPrem <prem.sy1952@gmail.com>
2023-09-04 22:12:36 +0700

    build from vite!
    hell yeah

--- templates/navbar/navbar.js
+++ templates/navbar/navbar.js
@@ -1,6 +1,6 @@
 async function nav_main() {
     const nav = document.querySelector('nav');
-    const html = await (await fetch('../templates/navbar/navbar.html')).text();
+    const html = await (await fetch('templates/navbar/navbar.html')).text();
     nav.innerHTML = html;
 }
 
