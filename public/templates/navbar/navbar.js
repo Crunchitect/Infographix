74a2274f202f7b484029ac93b124cdc2fb4947c3
PremPremPremPrem <prem.sy1952@gmail.com>
2023-09-05 19:42:05 +0700

    Hopefully vite works
    please

---
+++ public/templates/navbar/navbar.js
@@ -0,0 +1,7 @@
+async function nav_main() {
+    const nav = document.querySelector('nav');
+    const html = await (await fetch('templates/navbar/navbar.html')).text();
+    nav.innerHTML = html;
+}
+
+nav_main();
