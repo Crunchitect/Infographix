4da0d340c29c618f7cd26081353283c67519de37
Cruchitect <prem.sy1952@gmail.com>
2023-11-17 12:35:43 +0700

    Respomsive UI

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div class="padding"></div>
-  <nav>
+  <nav v-if="viewWidth >= 960">
     <div class="left">
       <span class="pad"></span>
       <span class="fancy-text"><strong>Infographix</strong></span>
@@ -19,8 +19,46 @@ import { RouterLink, RouterView } from 'vue-router'
         <router-link to="/sign-up">Sign Up</router-link>
     </div>
   </nav>
+  <nav v-else>
+    <div class="left">
+      <span class="pad"></span>
+      <span class="fancy-text" v-if="viewWidth >= 540"><strong>Infographix</strong></span>
+      <span class="fancy-text" v-else><img src="./assets/icon.png" alt=""></span>
+      <span class="pad"></span>
+      <router-link to="/"><i class="fa-solid fa-house"></i></router-link>
+      <router-link to="/projects"><i class="fa-solid fa-folder-open"></i></router-link>
+      <router-link to="/about"><i class="fa-solid fa-info"></i></router-link>
+    </div>
+    <div class="right">
+        <router-link to="/tuto"><i class="fa-solid fa-arrow-right"></i></router-link>
+        <router-link to="/sign-in"><i class="fa-solid fa-right-to-bracket"></i></router-link>
+        <router-link to="/sign-up"><i class="fa-solid fa-user-plus"></i></router-link>
+    </div>
+  </nav>
   <router-view></router-view>
 </template>
+
+<script lang="ts">
+  export default {
+    name: "App",
+    data() {
+      return {
+        viewWidth: window.innerWidth
+      }
+    },
+    mounted() {
+      addEventListener('resize', this.getWidth);
+    },
+    unmounted() {
+      removeEventListener('resize', this.getWidth);
+    },
+    methods: {
+      getWidth() {
+        this.viewWidth = window.innerWidth;
+      }
+    }
+  }
+</script>
 
 <style scoped>
   .pad {
