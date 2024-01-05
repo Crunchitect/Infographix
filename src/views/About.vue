1b8d8ca077b083ebf4be908ada4bca5938c63dd7
Crunchitect <crunchitect@gmail.com>
2024-01-05 12:02:01 +0700

    !!!

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
     <p>hi, its crunchi.</p>
     <p>i'm been working solo on this website for 2 months now.</p>
-    <p>i'm just gonna say, don't expect rapid updates like this for a while, im burning out.</p>
+    <p>i'm just gonna say, don't expect rapid updates like this for a while.</p>
     <p>most of the work that will be done this month is backend stuff and refactoring, so it'll be easier to continue on this project.</p>
     <p>have fun, and expect me to use this page as a blog of my dev process for a while, come and check now and again, maybe.</p>
     <p>thanks for reading this</p>
