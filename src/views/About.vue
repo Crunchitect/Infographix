34589a03cdf13d1c0cb1b39a211cc7e648a7ea47
PremPremPremPrem <prem.sy1952@gmail.com>
2024-01-08 20:27:58 +0700

    aaaaaa

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,4 @@
 <template>
-    <p>hi, its crunchi.</p>
-    <p>i'm been working solo on this website for 2 months now.</p>
-    <p>i'm just gonna say, don't expect rapid updates like this for a while.</p>
-    <p>rapid updates like this came for 50-60 hour crunches, this website is mostly made in 4 days.</p>
-    <p>most of the work that will be done this month is backend stuff and refactoring, so it'll be easier to continue on this project.</p>
-    <p>have fun, and expect me to use this page as a blog of my dev process for a while, come and check now and again, maybe.</p>
-    <p>thanks for reading this</p>
-    <p>-- crunchi</p>
 </template>
 
 <style scoped>
